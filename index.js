a224e17e0fac1b584a3f5b1bc451e75190ab394f
Don Coleman <dcoleman@chariotsolutions.com>
2014-01-02 15:59:05 -0500

    tabs to spaces

--- index.js
+++ index.js
@@ -4,89 +4,89 @@
 //
 // (c) 2014 Don Coleman
 var noble = require('noble'),
-	rfduino = require('./rfduino'),
-	_ = require('underscore');
+    rfduino = require('./rfduino'),
+    _ = require('underscore');
 
 // TODO why does this need to be wrapped?
 var stop = function() {
-	noble.stopScanning();
+    noble.stopScanning();
 };
 
 noble.on('scanStart', function() {
-	console.log('Scan started');
-	setTimeout(stop, 5000);
+    console.log('Scan started');
+    setTimeout(stop, 5000);
 });
 
 noble.on('scanStop', function() {
-	console.log('Scan stopped');
+    console.log('Scan stopped');
 });
 
 var onDeviceDiscoveredCallback = function(peripheral) {
-	console.log('\nDiscovered Peripherial ' + peripheral.uuid);
+    console.log('\nDiscovered Peripherial ' + peripheral.uuid);
 
-	if (_.contains(peripheral.advertisement.serviceUuids, rfduino.serviceUUID)) {
-		console.log('RFduino is advertising \'' + rfduino.getAdvertisedServiceName(peripheral) + '\' service.');
+    if (_.contains(peripheral.advertisement.serviceUuids, rfduino.serviceUUID)) {
+        console.log('RFduino is advertising \'' + rfduino.getAdvertisedServiceName(peripheral) + '\' service.');
 
-		peripheral.on('connect', function() {
-			peripheral.discoverServices();
-		});
+        peripheral.on('connect', function() {
+            peripheral.discoverServices();
+        });
 
-		peripheral.on('disconnect', function() {
-			console.log('Disconnected');
-		});
+        peripheral.on('disconnect', function() {
+            console.log('Disconnected');
+        });
 
-		peripheral.on('servicesDiscover', function(services) {
+        peripheral.on('servicesDiscover', function(services) {
 
-			var rfduinoService;
+            var rfduinoService;
 
-			for (var i = 0; i < services.length; i++) {
-				if (services[i].uuid === rfduino.serviceUUID) {
-					rfduinoService = services[i];
-					break;
-				}
-			}
+            for (var i = 0; i < services.length; i++) {
+                if (services[i].uuid === rfduino.serviceUUID) {
+                    rfduinoService = services[i];
+                    break;
+                }
+            }
 
-			if (!rfduinoService) {
-				console.log('Couldn\'t find the RFduino service.');
-				return;
-			}
+            if (!rfduinoService) {
+                console.log('Couldn\'t find the RFduino service.');
+                return;
+            }
 
-			rfduinoService.on('characteristicsDiscover', function(characteristics) {
-				console.log('Discovered ' + characteristics.length + ' service characteristics');
+            rfduinoService.on('characteristicsDiscover', function(characteristics) {
+                console.log('Discovered ' + characteristics.length + ' service characteristics');
 
-				var receiveCharacteristic;
+                var receiveCharacteristic;
 
-				for (var i = 0; i < characteristics.length; i++) {
-					if (characteristics[i].uuid === rfduino.receiveCharacteristicUUID) {
-						receiveCharacteristic = characteristics[i];
-						break;
-					}
-				}
+                for (var i = 0; i < characteristics.length; i++) {
+                    if (characteristics[i].uuid === rfduino.receiveCharacteristicUUID) {
+                        receiveCharacteristic = characteristics[i];
+                        break;
+                    }
+                }
 
-				if (receiveCharacteristic) {
-					receiveCharacteristic.on('read', function(data, isNotification) {
-						// temperature service sends a float
-						console.log(data.readFloatLE(0) + '\u00B0C');
-					});
+                if (receiveCharacteristic) {
+                    receiveCharacteristic.on('read', function(data, isNotification) {
+                        // temperature service sends a float
+                        console.log(data.readFloatLE(0) + '\u00B0C');
+                    });
 
-					console.log('Subscribing for temperature notifications');
-					receiveCharacteristic.notify(true);
-				}
+                    console.log('Subscribing for temperature notifications');
+                    receiveCharacteristic.notify(true);
+                }
 
-			});
+            });
 
-			rfduinoService.discoverCharacteristics();
+            rfduinoService.discoverCharacteristics();
 
-		});
+        });
 
-		peripheral.connect();
-	}
+        peripheral.connect();
+    }
 };
 
 noble.on('stateChange', function(state) {
-	if (state === 'poweredOn') {
-		noble.startScanning([rfduino.serviceUUID], false);
-	}
+    if (state === 'poweredOn') {
+        noble.startScanning([rfduino.serviceUUID], false);
+    }
 });
 
 noble.on('discover', onDeviceDiscoveredCallback);
